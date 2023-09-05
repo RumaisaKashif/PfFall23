1947ed237d2b3f1fe62ad06643bf06115d5ec7c6
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-05 10:40:35 +0500

    Update RumaisaQ1.c

--- Labs/L03/RumaisaQ1.c
+++ Labs/L03/RumaisaQ1.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main() {
+	printf("lets find the volume of Ahmed's container\n");
+	printf("lets take dimensions\n");
 	int h;
-	h=20;
 	int l;
-	l=10;
 	int w;
-	w=10;
 	int v;
+	printf("enter height:");
+	scanf("%d", &h);
+	printf("enter width:");
+	scanf("%d", &w);
+	printf("enter length:");
+	scanf("%d", &l);
 	v=h*l*w;
 	printf("volume of Ahmed's container is %d" , v);
+	return 1;
 
 }
