8c78e2a9ac8a5a089d129234e581ebc09d3e21a2
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-12 06:41:49 +0500

    Add files via upload
    RumaisaQ3.c

---
+++ Labs/L03/main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  helloworldrumi
+//
+//  Created by Rumaisa Kashif on 12/09/2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    printf("lets find the sale price of an item!\n");
+    float a;
+    printf("enter original price of item:\n");
+    scanf("%f" , &a);
+    float d;
+    printf("enter percentage discount:\n");
+    scanf("%f",&d);
+    float sp;
+    sp=a*(1-0.01*d);
+    printf("the sale price is %f\n", sp);
+    return 1;
+}
+
