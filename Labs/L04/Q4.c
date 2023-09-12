0164b9699c5c8021b21f30e58654e3bc5356c9ad
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-12 22:31:47 +0500

    Create Q4.c

---
+++ Labs/L04/Q4.c
@@ -0,0 +1,22 @@
+//Q4 
+//Rumaisa Kashif 23k0744
+//code to find solution of quadratic equation
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    printf("using the quadratic formula to find roots of the equation f(X)=ax^2+bx+c where a,b,c are integers\n");
+    float a,b,c;
+    printf("enter a:\n");
+    scanf("%f", &a);
+    printf("enter b:\n");
+    scanf("%f", &b);
+    printf("enter c:\n");
+    scanf("%f", &c);
+    float x1,x2;
+    x1=((-1*b)/(2*a))+(sqrt((b*b)-(4*a*c))/(2*a));
+    x2=((-1*b)/(2*a))-(sqrt((b*b)-(4*a*c))/(2*a));
+    printf("the solutions to the equation are:\n");
+    printf("%f\n", x1);
+    printf("%f", x2);
+}
