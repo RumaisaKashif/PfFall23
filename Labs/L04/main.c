2e69809a12092eedcc7678d12be609ec5b7126ef
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-12 21:22:11 +0500

    Add files via upload

---
+++ Labs/L04/main.c
@@ -0,0 +1,33 @@
+
+
+//int main(int argc, const char * argv[]) {
+    // insert code here...
+    //printf("Hello, World!\n");
+    //return 0;
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int n;
+    printf("lets find sum of squares of n numbers!\n");
+    printf("enter a number n which is grater than 0 but less than 100:\n");
+    scanf("%d", &n);
+    int array[n];
+    int c=0;
+    while (c<n)
+    {
+        printf("enter integer:\n");
+        scanf("%d", &array[c]);
+        c=c+1;
+    }
+    c=0;
+    int sum=0;
+    while (c<n)
+    {
+        sum=sum+array[c]*array[c];
+        c=c+1;
+    }
+    printf("%d", sum);
+    return 0;
+}
