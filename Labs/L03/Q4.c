bdf0d2c532c2a5e2dcf52df5f68569314d5c8879
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-19 18:57:11 +0500

    Update Q4.c

--- Labs/L03/Q4.c
+++ Labs/L03/Q4.c
@@ -1,1 +1,20 @@
+//DISCOUNT CALCULATOR
+//Rumaisa Kashif
+//23K0744
+
+#include <stdio.h>
+
+int main()
+{
+    printf("lets calculate the sale price of a fat brains pipsquigs!\n");
+    printf("enter discount as a percentage:\n");
+    int d;
+    scanf("%d", &d);
+    printf("enter original price of product:\n");
+    float op;
+    scanf("%f", &op);
+    float sp;
+    sp=op*(1-0.01*d);
+    printf("sale price is %f\n", sp);
+}
 
