fb6603d29ca22dfd36283f23cd6fd4fb098c53eb
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-19 08:49:09 +0500

    Update Q1.c

--- Labs/L05/Q1.c
+++ Labs/L05/Q1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
 
+int main() {
+	printf("program to find whether a number is a multiple of 3 or not\n");
+	int n;
+	printf("enter number:\n");
+	scanf("%d", &n);
+	if(n%3==0) {
+		printf("number is a multiple of 3");
+	}
+	else {
+		printf("number is not a multiple of 3");
+	}
+}
