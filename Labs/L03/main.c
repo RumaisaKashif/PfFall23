27fe7c9c1c1eaa81e898ed1474073f3dd987716e
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-09-19 18:56:08 +0500

    Add files via upload

--- Labs/L03/main.c
+++ Labs/L03/main.c
@@ -1,39 +1,19 @@
-//program to make calculator on c
-//Rumaisa Kashif 23K0744
+//DISCOUNT CALCULATOR
+//Rumaisa Kashif
+//23K0744
+
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-    printf("lets use a simple calculator!\n ");
-    printf("enter an operation of choice from the following: +\t-\t*\t/\n");
-    char o;
-    scanf("%c", &o);
-    printf("enter number 1 and number 2\n");
-    int n1, n2;
-    scanf("%d", &n1);
-    scanf("%d", &n2);
-    int res;
-    switch (o)
-    {
-        case '+':
-            res=n1+n2;
-            printf("%d \n", res);
-            break;
-        case '-':
-            res=n1-n2;
-            printf("%d \n", res);
-            break;
-        case '*':
-            res=n1*n2;
-            printf("%d \n", res);
-            break;
-        case '/':
-            res=n1/n2;
-            printf("%d \n", res);
-            break;
-        default:
-            printf("wrong operator chosen, please review\n");
-            
-    }
+    printf("lets calculate the sale price of a fat brains pipsquigs!\n");
+    printf("enter discount as a percentage:\n");
+    int d;
+    scanf("%d", &d);
+    printf("enter original price of product:\n");
+    float op;
+    scanf("%f", &op);
+    float sp;
+    sp=op*(1-0.01*d);
+    printf("sale price is %f\n", sp);
 }
