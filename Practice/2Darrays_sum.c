567121a02f4f8d7a55ea0ba695f5f5c0bff21730
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-10-12 14:59:10 +0500

    Create 2Darrays_sum.c

---
+++ Practice/2Darrays_sum.c
@@ -0,0 +1,35 @@
+// Programmer: Rumaisa Kashif
+// Desc: program to print sum of 2 arrays
+// 12/10/23
+#include <stdio.h>
+
+int main() {
+    int arr1[2][2]={0};
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<2;j++)
+        {
+            printf("enter value for row %d and column %d:\n", i, j);
+            scanf("%d", &arr1[i][j]);
+        }
+    }
+    int arr2[2][2]={0};
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<2;j++)
+        {
+            printf("enter value for row %d and column %d:\n", i, j);
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+    int arr3[2][2]={0};
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<=i+1;j++)
+        {
+            arr3[i][j]=arr1[i][j]+arr2[i][j];
+            printf("%d\t", arr3[i][j]);
+        }
+        printf("\n");
+    }
+}
