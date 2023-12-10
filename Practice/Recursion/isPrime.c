c1ecd5112f60bb16d9fff5e7c12341ea7eb6886d
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-12-10 12:29:18 +0500

    Create isPrime.c

---
+++ Practice/Recursion/isPrime.c
@@ -0,0 +1,52 @@
+//recursively find if number is prime or not
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>  
+
+bool isPrime(int n, int i)
+{
+    if (n<=1)
+    {
+        return false;
+    }
+    else if (n==2)
+    {
+        return true;
+    }
+    else
+    {
+        if (n%i==0)
+        {
+            return false;
+        }
+        else
+        {
+            if (i < 2) // Corrected condition
+            {
+                return true;
+            }
+            else
+            {
+                return isPrime(n,i-1);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int num;
+    printf("ENTER NUM:\n");
+    scanf("%d", &num);
+    int i=(int)sqrt(num)+1;
+    if (!isPrime(num,i))
+    {
+        printf("not prime\n");
+    }
+    else 
+    {
+        printf("prime\n");
+    }
+}
