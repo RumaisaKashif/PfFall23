80a72cac78895e95576c7b4a74562ff3b1ba4e21
RumaisaKashif <142867414+RumaisaKashif@users.noreply.github.com>
2023-12-14 11:39:26 +0500

    Update Q1.c

--- Labs/L10/Q1.c
+++ Labs/L10/Q1.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define MAX_SIZE 10
+
+// Universal function to print different arrays
+void printArray(void* arr, int size, char type) 
+{
+    int i;
+    switch(type) 
+    {
+        case 'i':
+            for (i = 0; i < size; i++) 
+            {
+                printf("%d, ", ((int*)arr)[i]);
+            }
+            break;
+
+        case 'c':
+            for (i = 0; i < size; i++) 
+            {
+                printf("%c, ", ((char*)arr)[i]);
+            }
+            break;
+
+        case 'f':
+            for (i = 0; i < size; i++) 
+            {
+                printf("%f, ", ((float*)arr)[i]);
+            }
+            break;
+
+        default:
+            break;
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    // Declare and initialize diffferent types of arrays
+    int a[10] = {0};
+    char c[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+    float f[10] = {0};
+
+    // Print arrays by calling function
+    printf("Array of integers: ");
+    printArray(a, MAX_SIZE, 'i');
+
+    printf("Array of characters: ");
+    printArray(c, MAX_SIZE, 'c');
+
+    printf("Array of floats: ");
+    printArray(f, MAX_SIZE, 'f');
+
+    return 0;
+}//end main
